709f31d15a51532fd667401a825202609fc9c39e
CJ Bryan <christianbryan@gmail.com>
2016-09-29 16:30:41 -0700

    Respond with a nicer error message.

--- app.rb
+++ app.rb
@@ -6,5 +6,7 @@ class ExportGifSound < Sinatra::Base
   # https://gifsound.com/?gif=i.imgur.com/bYmN79L.gif&v=bWMw4vE3J8s&s=12
   get '/' do
     ProcessVideoJob.perform_async(params)
+
+    "thanks, we're processing your request"
   end
 end
