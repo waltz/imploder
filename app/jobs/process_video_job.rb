3de880309e9da4c848ca35ca917eb2e8193c4e7b
CJ Bryan <christianbryan@gmail.com>
2016-10-02 14:42:10 -0700

    Force parsing of video id's to ints

--- app/jobs/process_video_job.rb
+++ app/jobs/process_video_job.rb
@@ -1,6 +1,6 @@
 class ProcessVideoJob < ApplicationJob
   def perform(video_id)
-    video = Video.find(video_id)
+    video = Video.find(video_id.to_i)
     destination_path = Rails.root.join('public', 'videos', "#{video.id}.mp4")
     system("./bin/converter #{video.gif_url} #{video.youtube_url} #{video.audio_start_delay} #{destination_path}")
     video.update!({ status: 'ready' })
