3972738b7a034ead64dac8cc7f8a372b9f3bb8bd
CJ Bryan <christianbryan@gmail.com>
2016-10-02 14:36:25 -0700

    Symlink the video directory in production.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@ server 'exportgifsound.com', user: 'deploy', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-
+set :linked_dirs, [ 'public/videos' ]
 
 # role-based syntax
 # ==================
