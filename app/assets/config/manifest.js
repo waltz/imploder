b81d380ae32a828f8eca2ca3c9d1745d2bdc0840
CJ Bryan <christianbryan@gmail.com>
2016-10-11 18:57:06 -0700

    Use a new font and restyle things a bit.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
-//= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
+//= link application.js
+//= link application.css
+//= link_tree ./fonts
+//= link_tree ./images
