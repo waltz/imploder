8a4cbf7ed6e1db52d9ab4fea908ead9934009a78
CJ Bryan <christianbryan@gmail.com>
2016-10-25 20:27:19 -0700

    Wire up Terraform and Ansible runs.

--- server.tf
+++ server.tf
@@ -1,5 +1,9 @@
 variable "digitalocean_token" {}
 
+output "ip" {
+    value = "${digitalocean_droplet.web.ipv4_address}"
+}
+
 provider "digitalocean" {
     token = "${var.digitalocean_token}"
 }
@@ -22,21 +26,7 @@ resource "digitalocean_droplet" "web" {
 
     provisioner "remote-exec" {
         inline = [
-		  "sudo apt-get install software-properties-common",
-		  "sudo yes | apt-add-repository ppa:ansible/ansible",
-		  "sudo apt-get update",
-		  "sudo yes | apt-get install ansible",
-        ]
-    }
-
-    provisioner "file" {
-        source = "server.yml"
-        destination = "~/server.yml"
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-		  "ansible-playbook -i \"localhost,\" -c local ~/server.yml"
-        ]
+		  "sudo yes | apt-get install python"
+	    ]
     }
 }
