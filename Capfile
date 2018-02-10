8dc449f2e4ae8b7dcf77baa179da359479225fcb
CJ Bryan <cj@deepyogurt.org>
2018-02-10 00:38:02 -0800

    Use Capistrano to cut releases.

--- Capfile
+++ Capfile
@@ -2,6 +2,10 @@ require "capistrano/setup"
 require "capistrano/deploy"
 require 'capistrano/rails'
 require 'capistrano/passenger'
+require 'capistrano/rbenv'
+
+require 'dotenv'
+Dotenv.load
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,10 @@
 lock '3.6.1'
 
+set :rbenv_type, :user
+set :rbenv_ruby, File.read('.ruby-version').strip
 set :user, 'deploy'
 set :application, 'exportgifsound.com'
 set :repo_url, 'https://github.com/waltz/exportgifsound.com.git'
+set :deploy_to, '/var/www/app'
+set :bundle_flags, '--deployment'
+set :bundle_env_variables, { nokogiri_use_system_libraries: 1, with_pg_lib: '/usr/lib' }
