95c12e1851495afa5ec556cf48e8c77654a612a4
CJ Bryan <cj@deepyogurt.org>
2018-02-09 20:08:09 -0800

    Lock down the Terraform plan.
    * First commit in a while.
    * Who the hell knows whats going on.

--- server.tf
+++ server.tf
@@ -1,32 +1,34 @@
 variable "digitalocean_token" {}
 
 output "ip" {
-    value = "${digitalocean_droplet.web.ipv4_address}"
+  value = "${digitalocean_droplet.web.ipv4_address}"
 }
 
 provider "digitalocean" {
-    token = "${var.digitalocean_token}"
+  token = "${var.digitalocean_token}"
 }
 
 resource "digitalocean_ssh_key" "default" {
-    name = "exportgifsound-test"
-    public_key = "${file("~/.ssh/id_rsa.pub")}"
+  name = "default"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
 resource "digitalocean_droplet" "web" {
-    image = "ubuntu-16-04-x64"
-    name = "exportgifsound-test"
-    region = "nyc3"
-    size = "512mb"
-    ssh_keys = [ "${digitalocean_ssh_key.default.id}" ]
+  image = "ubuntu-16-04-x64"
+  name = "exportgifsound-test"
+  region = "nyc3"
+  size = "512mb"
+  ssh_keys = ["${digitalocean_ssh_key.default.id}"]
 
-    connection {
-      private_key = "${file("~/.ssh/id_rsa")}"
-    }
+  connection {
+    type = "ssh"
+    user = "root"
+    private_key = "${file("~/.ssh/id_rsa")}"
+  }
 
-    provisioner "remote-exec" {
-      inline = [
-        "sudo yes | apt-get install python"
-      ]
-    }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yes | apt-get install python"
+    ]
+  }
 }
