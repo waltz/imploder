b658bdd4c95af21136f405e7386e322284557ccf
CJ Bryan <cj@deepyogurt.org>
2018-04-27 12:38:31 -0700

    Add FFmpeg to the Travis environment.

--- app/jobs/process_video_job.rb
+++ app/jobs/process_video_job.rb
@@ -2,7 +2,7 @@ class ProcessVideoJob < ApplicationJob
   def perform(video_id)
     video = Video.find(video_id.to_i)
     destination_path = Rails.root.join('tmp', "#{video.id}.mp4")
-    system("./bin/converter #{video.gif_url} #{video.youtube_url} #{video.audio_start_delay} #{destination_path} &>/dev/null")
+    system("./bin/converter #{video.gif_url} #{video.youtube_url} #{video.audio_start_delay} #{destination_path}")
     video.status = 'ready'
     video.clip = File.open(destination_path)
     video.save!
