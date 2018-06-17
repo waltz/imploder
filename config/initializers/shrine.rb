32218e87645bbb70ab0bb331174f6b3274c8a8d0
CJ Bryan <cj@deepyogurt.org>
2018-06-16 20:56:52 -0700

    Use S3 instead of DO Spaces.

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -12,8 +12,7 @@ elsif Rails.env.production?
     access_key_id: Rails.application.secrets.aws_access_key_id,
     secret_access_key: Rails.application.secrets.aws_secret_access_key,
     bucket: Rails.application.secrets.s3_bucket,
-    endpoint: 'https://nyc3.digitaloceanspaces.com',
-    region: 'nyc3',
+    region: 'us-west-1',
   }
   Shrine.storages = {
     cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
