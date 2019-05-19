b255c6cf751d33d6decfb8abc0b26e1362d9f249
CJ Bryan <cj@ministryofvelocity.com>
2019-05-19 13:01:43 -0700

    Switch FactoryBot syntax

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :video do
-    youtube_url "https://www.youtube.com/watch?v=i90EMCj98es"
-    gif_url "http://i.giphy.com/dswMLPwkJ3w3e.gif"
+    youtube_url { "https://www.youtube.com/watch?v=i90EMCj98es" }
+    gif_url { "http://i.giphy.com/dswMLPwkJ6w3e.gif" }
   end
 end
